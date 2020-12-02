95b154151460a92be81a66ce500bc68dc211e172
Justin Oakes <justin.t.oakes@gmail.com>
2020-12-02 10:16:11 -0600

    Map navigates to directions when double clicked

--- src/Map.js
+++ src/Map.js
@@ -12,21 +12,34 @@ const location = {
 };
 
 export default class Map extends Component {
-		  
+	constructor(props) {
+		super(props)
+		this.routeToMaps = this.routeToMaps.bind(this);
+	}
+
+	routeToMaps = () => window.location="https://www.google.com/maps/dir//Forest+Park,+OK/@35.4999509,-97.4544873,15.38z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87b217f8f75e2757:0x9d503000d3ebf255!2m2!1d-97.4461485!2d35.5042269";
+	
+	componentDidMount() {
+		console.log("I mounted");
+	}
+	
 	render() {
 		return (
 			<a id="map">
-				<div className={styles.container} id="map">
+				<div className={styles.container}>
 				<h3 className={styles.title}>Blueberry Hills</h3>
+				<p className={styles.directions}>Double click marker for directions</p>
 					<GoogleMapReact
 					  bootstrapURLKeys={{ key: "API Key"}}
 					  defaultCenter={location.center}
-					  defaultZoom={location.zoom}>
+					  defaultZoom={location.zoom}
+					  on>
 					  
 					  	<img src="images/pin.png"
 					  		lat={location.center.lat}
             				lng={location.center.lng}
            					 text="Venue"
+           					 onDoubleClick={this.routeToMaps}
 					  	/>
 					</GoogleMapReact>
 				</div>

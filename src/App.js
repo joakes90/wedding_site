43c39d9ea76bdb7aff5879514ade6bdd783f84bb
Justin Oakes <justin.t.oakes@gmail.com>
2020-12-18 09:13:44 -0600

    Updated date on save the date componet Updated location name and map on map component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export class Page extends Component {
 			return (
 				<div>
 					<NavBar/>
-					<a id="hero"><HeroBanner image="images/couple.jpg" title="Save the date." date="Sept 5th 2021" event="The wedding of Justin Thomas Oakes and Madeline Elizabeth Hudson"/></a>
+					<a id="hero"><HeroBanner image="images/couple.jpg" title="Save the date." date="Oct 17th 2021" event="The wedding of Justin Thomas Oakes and Madeline Elizabeth Hudson"/></a>
 					<a id="map"><Map/></a>
 					<a id="photos"><PlaceHolder title="Photos" message="Check back here after the big day for pics of the event"/></a>
 					<a id="RSVP"><RSVP/></a>
--- src/Map.js
+++ src/Map.js
@@ -5,10 +5,10 @@ import GoogleMapReact from 'google-map-react';
 
 const location = {
 	center: {
-	  lat: 35.6596479,
-	  lng: -97.4587054
+	  lat: 35.6174202,
+	  lng: -97.6141328
 	},
-	zoom: 18
+	zoom: 14.75
 };
 
 export default class Map extends Component {
@@ -17,12 +17,12 @@ export default class Map extends Component {
 		this.routeToMaps = this.routeToMaps.bind(this);
 	}
 
-	routeToMaps = () => window.location="https://www.google.com/maps/dir//Forest+Park,+OK/@35.4999509,-97.4544873,15.38z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87b217f8f75e2757:0x9d503000d3ebf255!2m2!1d-97.4461485!2d35.5042269";
+	routeToMaps = () => window.location="https://www.google.com/maps/dir//Gaillardia+Country+Club,+5300+Gaillardia+Blvd,+Oklahoma+City,+OK+73142/@35.6156627,-97.6179349,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x87b20336a258306b:0x76000b815d44309f!2m2!1d-97.6157409!2d35.6156627";
 	
 	render() {
 		return (
 			<div className={styles.container}>
-				<h3 className={styles.title}>Blueberry Hills</h3>
+				<h3 className={styles.title}>Gaillardia Country Club</h3>
 				<p className={styles.directions}>Click marker for directions</p>
 				<div className={styles.mapContainer}>
 					<GoogleMapReact
@@ -32,8 +32,8 @@ export default class Map extends Component {
 					  on>
 				  
 						<img src="images/pin.png"
-							lat={location.center.lat}
-							lng={location.center.lng}
+							lat={location.center.lat + 0.003}
+							lng={location.center.lng - 0.001}
 							 text="Venue"
 							 onClick={this.routeToMaps}
 							 alt="Map marker"

f613b4114da2e88a2cc3df1b04a4d341a972d2d9
Justin Oakes <justin.t.oakes@gmail.com>
2021-01-18 13:49:43 -0600

    added FAQ section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Map from './Map.js';
 import NavBar from './NavBar.js';
 import HeroBanner from './HeroBanner.js';
 import RSVP from './RSVP.js';
+import FAQ from './FAQ.js';
 import PhotoGrid from './PhotoGrid.js';
 import Footer from './Footer.js';
 import HashTag from './HashTag.js';
@@ -16,6 +17,7 @@ export class Page extends Component {
 					<a id="hero"><HeroBanner image="images/couple_croped_gray.jpg" title="Save the date." date="Oct 17th 2021" event="The wedding of Justin Oakes and Madeline Hudson"/></a>
 					<a id="map"><Map/></a>
 					<a id="photos"><PhotoGrid title="Photos"/></a>
+					<a id="FAQ"><FAQ/></a>
 					<a id="RSVP"><RSVP/></a>
 					<Footer/>
 				</div>
